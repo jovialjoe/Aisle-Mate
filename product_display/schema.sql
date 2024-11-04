d555ae4cab700d8da1fb089926e728060fcda0e5
carol wang <carolw6@uci.edu>
2024-11-03 21:03:15 -0800

    added schema for database for store inventory

---
+++ product_display/schema.sql
@@ -0,0 +1,8 @@
+-- schema for database of store inventory
+
+CREATE TABLE Inventory (
+    product_id INTEGER NOT NULL PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    aisle_num TEXT NOT NULL
+) STRICT;

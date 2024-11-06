854348476a6d40356baaa7f6fa0b01a7416de882
carol wang <carolw6@uci.edu>
2024-11-05 20:17:25 -0800

    added bin_num to db schema

---
+++ product_display/schema.sql
@@ -0,0 +1,9 @@
+-- schema for database of store inventory
+
+CREATE TABLE Inventory (
+    product_id INTEGER NOT NULL PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    aisle_num INTEGER NOT NULL,
+    bin_num INTEGER NOT NULL
+) STRICT;

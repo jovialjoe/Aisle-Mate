df3039e5c4db784d5d7fdad222f15487157bd9bd
carol <88346343+carol3961@users.noreply.github.com>
2024-11-10 19:17:46 -0800

    Delete product_display/schema.sql

--- product_display/schema.sql
+++
@@ -1,9 +0,0 @@
--- schema for database of store inventory
-
-CREATE TABLE Inventory (
-    product_id INTEGER NOT NULL PRIMARY KEY,
-    product_name TEXT NOT NULL,
-    quantity INTEGER NOT NULL,
-    aisle_num INTEGER NOT NULL,
-    bin_num INTEGER NOT NULL
-) STRICT;
